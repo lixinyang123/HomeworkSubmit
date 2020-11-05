9aa4d8846c0cedfd7ed14c8756363da284c0da09
root <root@LLLXY.localdomain>
2020-11-06 01:08:33 +0800

    完善界面

---
+++ src/static/js/index.js
@@ -0,0 +1,18 @@
+function switchTheme(){
+    if(document.body.className == "light"){
+        document.body.className = "dark";
+    }
+    else{
+        document.body.className = "light";
+    }
+}
+
+function initTheme(){
+    var time = new Date().getHours();
+    if(time > 6 && time < 19){
+        document.body.className = "light";
+    }
+    else{
+        document.body.className = "dark";
+    }
+}
