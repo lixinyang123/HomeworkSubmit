4793df5cc8601a1489ff165bca51bed2afcc9860
root <root@LLLXY.localdomain>
2020-11-08 15:20:56 +0800

    完善脚本注入

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,3 +1,37 @@
+function request(href,callback){
+    fetch(href,{
+        method:"GET"
+    }).then((res)=>{
+        res.text().then((data)=>{
+            callback(data);
+        })
+    });
+}
+
+// 页内导航
+function navigation(name){
+    let url = "/static/view/" + name + ".html";
+
+    request(url,(html)=>{
+        document.querySelector("#main").innerHTML = html;
+        inject();
+        url = "#"+name;
+        history.pushState({page: name},name,url);
+    });
+}
+
+// 注入页内脚本
+function inject(){
+    document.querySelectorAll("#main > script").forEach(element => {
+        let src = element.getAttribute("src");
+        request(src,(data) => {
+            if(data==undefined){
+                console.log("inject error");
+            }
+        });
+    });
+}
+
 // 切换主题
 function switchTheme(){
     if(document.body.className == "light"){
@@ -17,33 +51,6 @@ function initTheme(){
     else{
         document.body.className = "dark";
     }
-}
-
-// 页内导航
-function navigation(name){
-    let url = "/static/view/" + name + ".html";
-
-    fetch(url,{
-        method:"GET"
-    }).then((res)=>{
-        res.text().then((html)=>{
-            document.querySelector("#main").innerHTML = html;
-            inject();
-            url = "#"+name;
-            history.pushState({page: name},name,url);
-        })
-    });
-}
-
-function inject(){
-    document.querySelectorAll("#main > script").forEach(element => {
-        let src = element.getAttribute("src");
-        request(src,(data) => {
-            if(data==undefined){
-                console.log("inject error");
-            }
-        });
-    });
 }
 
 function init(){
